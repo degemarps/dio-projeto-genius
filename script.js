0e267ff14a1ae91d662c365d886979ae88f14462
degemarps <degemarpereira@gmail.com>
2021-12-22 11:49:34 -0300

    Adiçao de mais funçoes do jogo

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('green');
 const yellow = document.querySelector('yellow');
 
+// Cria ordem aleatória de cores
 const shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random * 4);
   order[order.length] = colorOrder;
@@ -23,6 +24,7 @@ const shuffleOrder = () => {
   }
 };
 
+// Acende a próxima cor
 const lightColor = (element, number) => {
   number = number * 500;
   setTimeout(() => {
@@ -31,4 +33,66 @@ const lightColor = (element, number) => {
   setTimeout(() => {
     element.classList.remove('selected');
   });
+};
+
+// Checa se os botões clicados são os mesmos da ordem gerada no jogo
+const checkOrder = () => {
+  for (let i in clickedOrder) {
+    if (clickedOrder[i] !== order[i]) {
+      gameOver();
+      break;
+    } 
+  }
+  if (clickedOrder.length === order.length) {
+    alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+    nextLevel();
+  }
+};
+
+// Clique do usuário
+const click = (color) => {
+  clickedOrder[clickedOrder.length] = color;
+  createColorElement(color).classList.add('selected');
+
+  setTimeout(() => {
+    createColorElement(color).classList.add('selected');
+  });
+
+  checkOrder();
+};
+
+// Retorna a cor
+const createColorElement = (color) => {
+  if (color === 0) {
+    return green;
+  } else if (color === 1) {
+    return red;
+  } else if (color === 2) {
+    return yellow;
+  } else if (color === 3) {
+    return blue;
+  }
+};
+
+// Próximo nível do jogo
+const nextLevel = () => {
+  score += 1;
+  shuffleOrder();
+};
+
+// Caso o jogador perca
+const gameOver = () => {
+  alert(`Pontuação: ${score}\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+  order = [];
+  clickedOrder = [];
+
+  playGame();
+};
+
+// Iniciar o jogo
+const playGame = () => {
+  alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+  score = 0;
+
+  nextLevel();
 };
